e8b3ad5000158a2e553f7f221e7462216966f007
Susanna Huang <dearsusannahuang@gmail.com>
2017-01-25 18:24:35 -0500

    ch 2 comp

--- src/pledge.js
+++ src/pledge.js
@@ -9,17 +9,22 @@ function $Promise(executor){
  this._state = 'pending';
  this._handlerGroups = [];
 
- //console.log('#######', this._handlerGroups);
  if(typeof executor !== 'function'){
   throw new Error('error');
  }
-   var resolve = function(data){
-     return this._internalResolve(data);
-   };
-   var reject = function(data){
-     return this._internalReject(data);
-   };
-   executor(resolve.bind(this), reject.bind(this))
+  //  var resolve = function(data){
+  //    return this._internalResolve(data);
+  //  };
+  //  var reject = function(data){
+  //    return this._internalReject(data);
+  //  };
+
+   //internal methods
+   this._internalResolve = this._internalResolve.bind(this);
+   this._internalReject = this._internalReject.bind(this);
+   this._callHandlers = this._callHandlers.bind(this);
+
+   executor(this._internalResolve, this._internalReject)
 }
 
 $Promise.prototype._internalResolve = function(data){
@@ -27,6 +32,7 @@ $Promise.prototype._internalResolve = function(data){
     this._state = 'fulfilled';
     this._value = data;
   }
+  this._callHandlers();
 };
 
 $Promise.prototype._internalReject = function(data){
@@ -34,6 +40,7 @@ $Promise.prototype._internalReject = function(data){
     this._state = 'rejected';
     this._value = data;
   }
+  this._callHandlers();
 };
 
 
@@ -49,30 +56,29 @@ $Promise.prototype.then = function(data, err){
   this._handlerGroups.push(obj);
 
   if(this._state === 'fulfilled') {
-    this._callHandler(this._handlerGroups[this._handlerGroups.length -1]);
+    this._callHandlers();
   }
-  else {
-    
-  }
+
 
 };
 
-$Promise.prototype._callHandler = function(obj){
+$Promise.prototype._callHandlers = function(){
 
-  // console.log(this._handlerGroups[this._handlerGroups.length -1].successCb(this._value));
+var handler;
 
+  while(this._handlerGroups.length) {
+    handler = this._handlerGroups.shift();
 
-  obj.successCb(this._value);
+      if(this._state === 'fulfilled') {
+       handler.successCb(this._value);
+      }
+
+      if(this._state === 'rejected') {
+        handler.rejectCb(this._value);
+      }
+  }
 
 };
-
-
-
-  // if (this._state === 'fulfilled' && typeof data === 'function'){
-  //   this._handlerGroups.map(function(executor){
-  //     return data(this._value);
-  //   })
-  // }
 
 
 
